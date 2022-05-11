52df5af1a7f44d634882a73a7113ab3933270513
Travis Ralston <travisr@matrix.org>
2022-05-10 20:21:15 -0600

    Add a module installer script

---
+++ src/modules.ts
@@ -0,0 +1,13 @@
+
+/*
+ * THIS FILE IS AUTO-GENERATED
+ * You can edit it you like, but your changes will be overwritten,
+ * so you'd just be trying to swim upstream like a salmon.
+ * You are not a salmon.
+ */
+
+import { RuntimeModule } from "@matrix-org/react-sdk-module-api/lib/RuntimeModule";
+import Module1 from "@vector-im/element-web-ilag-module";
+
+export const INSTALLED_MODULES: RuntimeModule[] = [];
+INSTALLED_MODULES.push(Module1);
